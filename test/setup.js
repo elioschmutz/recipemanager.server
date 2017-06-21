fcdc09ac340de73ac029db8b10ce4d7a588662ff
elio.schmutz <elio.schmutz@gmail.com>
2017-06-21 23:19:03 +0200

    Remove mongodb-runner before and after test hoock because it is already done by the pretest and posttest script

--- test/setup.js
+++ test/setup.js
@@ -7,14 +7,8 @@ var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiHttp);
 chai.use(chaiAsPromised);
 
-// Starts a local mongodb to test against.
-before(require('mongodb-runner/mocha/before'));
-
 before(function() {
   return env.db.connect(config.db.url).then(() => {
     return env.app.bootstrap();
     });
 });
-
-// Shuts down the local mongodb.
-after(require('mongodb-runner/mocha/after'));
