f30eceabf542f2f2662057d5d40c096e79e19653
elio.schmutz <elio.schmutz@gmail.com>
2017-06-24 22:47:26 +0200

    Use example emails in tests

--- test/controllers/test_users.js
+++ test/controllers/test_users.js
@@ -12,7 +12,7 @@ describe('User', () => {
       it('it should GET all the users', () => {
         users = [];
         for (let i = 0; i < 3; i++) {
-          users.push(new User({username: `user.${i}@example.com`, password: '1234'}).save());
+          users.push(new User({username: `james.${i}@example.com`, password: '1234'}).save());
         }
         return Promise.all(users).then((values) => {
           return chai.request(app.get())
@@ -29,9 +29,9 @@ describe('User', () => {
     it('it should create a new user', () => {
       return chai.request(app.get())
         .post('/api/users')
-        .send({username: 'max.muster@example.com', password: '1234'})
+        .send({username: 'james.bond@example.com', password: '1234'})
         .then(function(res) {
-          assert.equal(res.body.username, 'max.muster@example.com');
+          assert.equal(res.body.username, 'james.bond@example.com');
         });
     });
   });
--- test/models/test_user.js
+++ test/models/test_user.js
@@ -19,29 +19,29 @@ describe('User Model', () => {
 
     it('password is required', () => {
       return assert.isRejected(
-        new User({username: 'elio.schmutz@msn.com'}).save());
+        new User({username: 'james.bond@example.com'}).save());
     });
 
     it('password is encrypted', () => {
       return assert.doesNotBecome(
-        new User({username: 'elio.schmutz@msn.com', password: '1234'}).save(),
+        new User({username: 'james.bond@example.com', password: '1234'}).save(),
         '1234');
     });
 
     it('password validation with correct password returns true', () => {
-      return new User({username: 'elio.schmutz@msn.com', password: '1234'}).save().then((user) => {
+      return new User({username: 'james.bond@example.com', password: '1234'}).save().then((user) => {
         return assert.becomes(user.validatePassword('1234'), true);
       });
     });
 
     it('password validation with invalid password returns false', () => {
-      return new User({username: 'elio.schmutz@msn.com', password: '1234'}).save().then((user) => {
+      return new User({username: 'james.bond@example.com', password: '1234'}).save().then((user) => {
         return assert.becomes(user.validatePassword('xxxx'), false);
       });
     });
 
-      return new User({username: 'elio.schmutz@msn.com', password: '1234'}).save().then((user) => {
     it('json representation does not return password', () => {
+      return new User({username: 'james.bond@example.com', password: '1234'}).save().then((user) => {
         return assert.notProperty(user.toJSON(), 'password');
       });
     });
